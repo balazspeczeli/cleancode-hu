418335ac8d29dfde30411d135402bd14e80d1fc1
Balazs Peczeli <balazspeczeli87@gmail.com>
2022-03-09 01:10:55 +0100

    Refactor Icon component

--- src/components/ui/Icon/Icon.tsx
+++ src/components/ui/Icon/Icon.tsx
@@ -1,10 +1,26 @@
-import dynamic from 'next/dynamic';
 import { SVGAttributes } from 'react';
+import GitHub from 'assets/icons/github.svg';
+import Home from 'assets/icons/home.svg';
+import JavaScript from 'assets/icons/javascript.svg';
+import React from 'assets/icons/react.svg';
+import TypeScript from 'assets/icons/typescript.svg';
+
+const iconTypes = {
+  github: GitHub,
+  home: Home,
+  javascript: JavaScript,
+  react: React,
+  typescript: TypeScript,
+};
 
 interface IconProps extends SVGAttributes<SVGElement> {
   name: string;
 }
+
 export const Icon = ({ name, ...props }: IconProps) => {
-  const Icon = dynamic(() => import(`assets/icons/${name}.svg`));
+  const Icon = iconTypes[name as keyof typeof iconTypes];
+  if (!Icon) {
+    throw new Error(`Unknown icon (${name})`);
+  }
   return <Icon {...props} />;
 };
