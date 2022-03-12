4b92c41565b5c7e7dbfd28f30d43b70082824883
Balazs Peczeli <balazspeczeli87@gmail.com>
2022-03-12 13:53:13 +0100

    Add CSS and HTML logos

--- src/components/ui/Icon/Icon.tsx
+++ src/components/ui/Icon/Icon.tsx
@@ -1,13 +1,17 @@
 import { SVGAttributes } from 'react';
+import CSS from 'assets/icons/css.svg';
 import GitHub from 'assets/icons/github.svg';
 import Home from 'assets/icons/home.svg';
+import HTML from 'assets/icons/html.svg';
 import JavaScript from 'assets/icons/javascript.svg';
 import React from 'assets/icons/react.svg';
 import TypeScript from 'assets/icons/typescript.svg';
 
 const iconTypes = {
+  css: CSS,
   github: GitHub,
   home: Home,
+  html: HTML,
   javascript: JavaScript,
   react: React,
   typescript: TypeScript,
