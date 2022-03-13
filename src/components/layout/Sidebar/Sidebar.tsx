91c1000732138d9f8f54876a76349aaebd7502f0
Balazs Peczeli <balazspeczeli87@gmail.com>
2022-03-13 15:06:31 +0100

    Add body-scroll-lock

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+import { useEffect, useRef } from 'react';
 import classNames from 'classnames';
 import { useStore } from 'hooks/useStore';
 import { Backdrop } from './Backdrop';
@@ -6,14 +8,25 @@ import styles from './Sidebar.module.scss';
 import { Title } from './Title';
 
 export const Sidebar = () => {
+  const sidebarRef = useRef<HTMLElement>(null);
+  const sidebarElement = sidebarRef.current as HTMLElement;
   const isSidebarOpen = useStore((state) => state.isSidebarOpen);
   const closeSidebar = useStore((state) => state.closeSidebar);
+
+  useEffect(() => {
+    if (isSidebarOpen) {
+      disableBodyScroll(sidebarElement);
+    } else {
+      enableBodyScroll(sidebarElement);
+    }
+  }, [sidebarElement, isSidebarOpen]);
 
   return (
     <aside
       className={classNames(styles.component, {
         [styles.open]: isSidebarOpen,
       })}
+      ref={sidebarRef}
     >
       <Backdrop isSidebarOpen={isSidebarOpen} closeSidebar={closeSidebar} />
       <Title />
