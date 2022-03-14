b79bd825dc48b389b1088ff0d053323871f5ac17
Balazs Peczeli <balazspeczeli87@gmail.com>
2022-03-15 00:06:37 +0100

    Add Hero to components/ui/index.ts

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,5 @@
 export { CodeSnippet } from './CodeSnippet';
+export { Hero } from './Hero';
 export { Icon } from './Icon';
 export { Note } from './Note';
 export { PageHeader } from './PageHeader';
