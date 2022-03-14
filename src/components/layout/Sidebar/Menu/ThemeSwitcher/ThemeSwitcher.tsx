b397300f6371312e6c858f9971c1649d700e1e1c
Balazs Peczeli <balazspeczeli87@gmail.com>
2022-03-14 10:46:34 +0100

    Add "Close sidebar on theme switch"

--- src/components/layout/Sidebar/Menu/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/layout/Sidebar/Menu/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,13 +1,20 @@
 import { Theme } from 'types';
+import { useStore } from 'hooks/useStore';
 import { useTheme } from 'hooks/useTheme';
 import { Icon } from 'components/ui';
 import styles from './ThemeSwitcher.module.scss';
 
 export const ThemeSwitcher = () => {
   const { theme, toggleTheme } = useTheme();
+  const closeSidebar = useStore((state) => state.closeSidebar);
+
+  const handleClick = () => {
+    toggleTheme();
+    closeSidebar();
+  };
 
   return (
-    <a onClick={toggleTheme} className={styles.component}>
+    <a onClick={handleClick} className={styles.component}>
       {theme === Theme.Dark && (
         <>
           <Icon name="theme-light" />
