1e7eeeabcbaccc98a4d01a0af9715cdde541e543
Balazs Peczeli <balazspeczeli87@gmail.com>
2022-03-15 10:09:29 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+/* eslint-disable  @typescript-eslint/no-var-requires */
+const miniSVGDataURI = require('mini-svg-data-uri');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,6 +10,31 @@ const nextConfig = {
       issuer: /\.tsx$/,
       use: ['@svgr/webpack'],
     });
+
+    // For inlining SVG images used in SCSS stylesheets
+    // https://dev.to/smelukov/webpack-5-asset-modules-2o3h
+    config.module.rules.push({
+      test: /\.svg/,
+      issuer: /\.scss$/,
+      type: 'asset/inline',
+      generator: {
+        dataUrl(content) {
+          content = content.toString();
+          return miniSVGDataURI(content);
+        },
+      },
+      use: 'svgo-loader',
+    });
+
+    // Temporary workaround for "generator has an unknown property 'filename'."
+    // https://github.com/vercel/next.js/issues/34501#issuecomment-1046655345
+    if (config.module.generator?.asset?.filename) {
+      if (!config.module.generator['asset/resource']) {
+        config.module.generator['asset/resource'] =
+          config.module.generator.asset;
+      }
+      delete config.module.generator.asset;
+    }
 
     return config;
   },
